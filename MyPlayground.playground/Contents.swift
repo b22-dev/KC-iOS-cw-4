13e4e1cd996e59633ba72470e59c9f1c4802fe96
b22-dev <99684336+b22-dev@users.noreply.github.com>
2022-08-06 13:11:34 +0300

    casting

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -16,3 +16,10 @@ if age <= 19 && age >= 13 {
         print("لايمكنك المشاركة في الكويت تبرمج")
     }
 
+
+var textName = "8"
+var number = 4
+var doubleNumb : Double
+
+
+doubleNumb = (Double(textName) ?? 0) + Double(number)
