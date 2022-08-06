8b26d5a76197fbc2c8da9e61573e479e53d8cd0c
b22-dev <99684336+b22-dev@users.noreply.github.com>
2022-08-06 13:02:49 +0300

    ageKuwaitCodess

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var name = "Ahmed"
+var birthdayYear = 2006
+var currentYear = 2022
+var age : Int
+
+age = currentYear - birthdayYear
+
+print("Hello my name is \(name) and my age is \(age)")
+
+if age <= 19 || age >= 13 {
+    print(" نعم يمكنك المشاركة في الكويت تبرمج")
+}
+    else {
+        print("لايمكنك المشاركة في الكويت تبرمج")
+    }
+
